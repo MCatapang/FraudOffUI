853359c473ee002785b3af0792a11f6509cdc6de
Michael <maocatap@gmail.com>
2024-05-27 17:42:00 -0700

    Modularized the home page card contents into their own constant

--- components/card-grid.tsx
+++ components/card-grid.tsx
@@ -1,28 +1,38 @@
 import LinkCard from "./link-card";
 
 export default function CardGrid() {
+  const cards: LinkCardParams[] = [
+    {
+      href: "/the-breach",
+      title: "The Breach",
+      description: "Read up on the breaking news without breaking any firewalls."
+    },
+    {
+      href: "/phishing-school",
+      title: "Phishing School",
+      description: "Hook, line, and sinker! Learn how to protect yourself from evildoers."
+    },
+    {
+      href: "/rate-a-risk",
+      title: "Rate-A-Risk",
+      description: "Check out what's hot - and what's not - with regards to cybersecurity."
+    },
+    {
+      href: "/tech-threads",
+      title: "Tech Threads",
+      description: "See what the community has to say about various topics."
+    }
+  ]
+
   return (
     <div className="grid text-center lg:max-w-6xl lg:w-full lg:mb-0 grid-cols-2 lg:grid-cols-4 lg:text-left">
-      <LinkCard 
-        href='/the-breach'
-        title='The Breach'
-        description='Read up on the breaking news without breaking any firewalls.'
-      />
-      <LinkCard 
-        href='/phishing-school'
-        title='Phishing School'
-        description='Hook, line, and sinker! Learn how to protect yourself from evildoers!'
-      />
-      <LinkCard 
-        href='/rate-a-tech'
-        title='Rate-A-Tech'
-        description=''
-      />
-      <LinkCard 
-        href='/forums'
-        title='Forums'
-        description='Check out the code that runs the show behind the scenes.'
-      />
+      {cards.map(c => 
+        <LinkCard
+          href={c.href}
+          title={c.title}
+          description={c.description}
+        />
+      )}
     </div>
   )
 }
